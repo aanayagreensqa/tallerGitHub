11be1d39aa465123727a12785caa7bc5c9feabbb
Adrian Anaya <aanaya@greensqa.com>
2025-05-21 18:31:24 -0500

    Agregado caso de prueba para login exitoso

---
+++ tests/test_login_correcto.py
@@ -0,0 +1,8 @@
+def test_login_correcto(): 
+usuario = "admin" 
+clave = "123" 
+assert login(usuario, clave) == "Login exitoso" 
+def login(usuario, clave): 
+if usuario == "admin" and clave == "123": 
+return "Login exitoso" 
+return "Error" 
